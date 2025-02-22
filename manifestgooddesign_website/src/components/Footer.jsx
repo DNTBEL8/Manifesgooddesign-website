0689604579ad4f1a8176382903c6885822fae3bb
DNTBEL8 <info@manifestgooddesign.com>
2025-02-21 18:18:20 -0800

    Add files via upload

---
+++ manifestgooddesign_website/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2025 Manifest Good Design. All Rights Reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
